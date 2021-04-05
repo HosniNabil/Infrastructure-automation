c228175b04713aac2a209ca788eaa89c1fffc13e
Nabil Hosni <hsoninabil94@gmail.com>
2021-04-05 16:41:25 +0100

    change cloud init iso name

--- collections/ansible_collections/hosninabil/kvm_terraform/roles/centos/files/main.tf
+++ collections/ansible_collections/hosninabil/kvm_terraform/roles/centos/files/main.tf
@@ -18,7 +18,7 @@ data "template_file" "network_config" {
 }
 
 resource "libvirt_cloudinit_disk" "commoninit" {
-  name           = "commoninit.iso"
+  name           = "commoninitcentos.iso"
   user_data      = data.template_file.user_data.rendered
   network_config = data.template_file.network_config.rendered
   pool           = "default"
